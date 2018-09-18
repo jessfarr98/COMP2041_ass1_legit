1fa95f95a3ae477689b0db83d0c0d099ae7a40bd
jessfarr98 <z5113609@ad.unsw.edu.au>
2018-09-18 12:01:40 +1000

    initialising the git repository and start of the program

---
+++ legit.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+#created by z5113609
+#COMP2041: Software Construction, semester 2, 2018
+#program called legit.pl which is a subset of the git version control system
+
+use strict;
+use warnings;
+
+#Subset 0 content
+
+
