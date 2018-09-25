2369ce3ca9cfa76313e0bd4ce3d8407c0fc1c36c
jessfarr98 <z5113609@ad.unsw.edu.au>
2018-09-25 21:25:34 +1000

    created a rough copy of the add function where .index has the same parent directory as .legit

--- legit.pl
+++ legit.pl
@@ -6,6 +6,7 @@
 
 use strict;
 use warnings;
+use File::Copy;
 
 #Subset 0 implementations
 
@@ -17,33 +18,67 @@ use warnings;
 if ($ARGV[0] eq "init") {
 	init();
 } elsif ($ARGV[0] eq "add") {
+	my @files = @ARGV[1..$#ARGV];
+
+	add(@files);
+
+} elsif ($ARGV[0] eq "commit") {
+
+
+
+} elsif ($ARGV[0] eq "log") {
+
+
+
+} elsif ($ARGV[0] eq "show") {
+
+
 
 }
-
 
 #A function to initialise the repository if required
 sub init {
 	#initialise the subdirectory called .legit
-	#my $subdir = "\.legit";
 	#the subdirectory already exists print an error message and exit with error status
-	if (-d "\.legit") {
-		#print "exist\n";
+	if (-d ".legit") {
+		#print "exists\n";
 		print "$0: error: .legit already exists\n";
 	} else {
 	#create the subdirectory
-		#print "not exist\n";
+		#print ".legit does not exist\n";
 		mkdir ".legit";
-		print "Initialised empty legit repository in .legit\n"
+		print "Initialised empty legit repository in .legit\n";
+
+		#in addition, create subdirectory for add called .index for the add files
+		mkdir ".index";
 	}
 }
 
-#a function to add files to the .legit repository
 #create a subdirectory called index and store the files in here
-#assume inly files are input. Don't worry about directories
+#assume only files are input. Don't worry about directories
 #think about using function signatures?
 sub add {
-	
+	my (@add_files) = @_;
+	print join "\n", @add_files;
+	print "\nmoving the files into the .index directory\n";
+	while(my $element = shift @add_files){
+		print "element is $element\n";
 
+		#create a copy of the file
+		my $temp = ".".$element;
+		open my $TEMP, '>', $temp or die "unable to write to the $temp file: $?\n";
+		open my $FILE, '<', $element or die "unable to open $element: $?\n";
+		foreach my $line(<$FILE>) {
+			print $TEMP $line;
+		}	
+		#move that copy into the .index directory
+		my $dir = ".index";
+		move "$temp", "$dir";
+
+
+		close $TEMP;
+		close $FILE
+	}
 }
 
 #commit adds the files in the index to the repository
@@ -51,6 +86,9 @@ sub add {
 #if the index is empty then say nothing to commit.
 #empty the index after committing
 sub commit {
+	
+
+
 
 }
 
